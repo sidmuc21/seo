54af3f073740279fa6249766abd3870b78faadea
SidritMucaj <sidmuc@htl-shkoder.com>
2026-03-04 12:51:32 +0100

    first co

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -4,6 +4,8 @@ import background from '$lib/assets/beach.jpg'
 <svelte:head>
 <title>Beaches 2026 - The best beaches of the year!</title>
 <meta name="description" content="We show the best beaches of the current year and help you organize trips">
+<meta property="og:title" content="Beaches 2026 - The best beaches of the year!">
+<meta property="og:image" content="{background}">
 </svelte:head>
 <header class="h-[500px] bg-center bg-cover flex items-center justify-center" style="background-image:url({background})">
 

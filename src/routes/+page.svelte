b647ff52c5abc850de2ce0f9639f41252f40a7dc
SidritMucaj <sidmuc@htl-shkoder.com>
2026-03-04 12:28:12 +0100

    first commit

---
+++ src/routes/+page.svelte
@@ -0,0 +1,16 @@
+<script>
+import background from '$lib/assets/beach.jpg'
+</script>
+<header class="h-[500px] bg-center bg-cover flex items-center justify-center" style="background-image:url({background})">
+
+<h1 class="text-5xl text-white">Electronic Devices</h1>
+
+</header>
+
+<main>
+
+</main>
+
+<footer>
+
+</footer>

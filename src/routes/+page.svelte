2d026b33863e84382f342abd66a6775f8a0f343a
SidritMucaj <sidmuc@htl-shkoder.com>
2026-03-04 12:40:27 +0100

    first comm

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,6 +1,10 @@
 <script>
 import background from '$lib/assets/beach.jpg'
 </script>
+<svelte:head>
+<title>Beaches 2026 - The best beaches of the year!</title>
+<meta name="description" content="We show the best beaches of the current year and help you organize trips">
+</svelte:head>
 <header class="h-[500px] bg-center bg-cover flex items-center justify-center" style="background-image:url({background})">
 
 <h1 class="text-5xl text-white">Electronic Devices</h1>
